f5f2350a6ceb879272a36614a7565d5262c3f238
Fai <lamkafai1997@gmail.com>
2023-05-05 21:30:03 +0800

    release(odoo): 20230505

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
+import tailwind from "@astrojs/tailwind";
 import yaml from '@rollup/plugin-yaml';
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
 import frontendistahtmlMinify from "@frontendista/astro-html-minify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://odoo-contrib.github.io',
+  base: '/static-pypi',
   vite: {
     plugins: [yaml()]
   },
